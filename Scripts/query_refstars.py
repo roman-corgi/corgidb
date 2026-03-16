36fb9b7ac65edc1d5386fa710c85fb5eeb5a9ee9
Dmitry Savransky <dsavransky@gmail.com>
2026-03-16 16:35:28 -0400

    updating refstar query and script

--- Scripts/query_refstars.py
+++ Scripts/query_refstars.py
@@ -1,9 +1,33 @@
 import requests
+import pandas
 
 url = "https://corgidb.sioslab.com/fetch_refs.php"
 response = requests.get(url, headers={"User-Agent": "XY"})
 
 if response.status_code == 200:
     data = response.json()
-    for d in data:
-        print(d)
+else:
+    print("Could not retrieve data")
+
+data = np.vstack(data).transpose()
+
+colnames = [
+    "st_name",
+    "main_id",
+    "ra",
+    "dec",
+    "spectype",
+    "sy_vmag",
+    "sy_imag",
+    "sy_dist",
+    "sy_plx",
+    "sy_pmra",
+    "sy_pmdec",
+    "st_radv",
+]
+
+out = {}
+for colname, col in zip(colnames, data):
+    out[colname] = col
+
+out = pandas.DataFrame(out)
--- frontend/fetch_refs.php
+++ frontend/fetch_refs.php
@@ -8,7 +8,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = 'SELECT main_id, spectype, sy_vmag, sy_imag from Stars where sy_caltype = "RefStar"';
+$sql = 'SELECT `st_name`, `main_id`, `ra`, `dec`, `spectype`, `sy_vmag`, `sy_imag`, `sy_dist`, `sy_plx`, `sy_pmra`, `sy_pmdec`, `st_radv` from Stars where sy_caltype = "RefStar"';
 $result = $conn->query($sql);
 $data = $result->fetch_all();
 
