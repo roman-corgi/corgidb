6845fec80c35a6ceffac480d00712bbe90f6a523
Nicholas Phillips <ntp28@cornell.edu>
2026-04-07 07:35:24 -0600

    Updating from feedback

--- tests/test_get_alias.py
+++ tests/test_get_alias.py
@@ -4,7 +4,7 @@ from corgidb import alias_check
 
 testList = ['Chi Uma', 'Omega Uma', '56 Uma', '58 Uma']
 start_time = time.perf_counter()
-all_data = alias_check(testList)
+all_data = alias_check.alias_check(testList)
 stop_time = time.perf_counter()
 print(all_data)
 elapsed_time = stop_time - start_time
